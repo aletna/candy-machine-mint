38392ba54c43af2cf93ff94ae13a4f22e0dba2a2
Dennis Antela Martinez <dennis.antela@gmail.com>
2021-09-08 14:32:10 -0700

    tst

--- src/NavbarCustom.tsx
+++ src/NavbarCustom.tsx
@@ -5,10 +5,8 @@ const NavbarCustom = (props: any) => {
   return (
     <div>
       <Navbar expand="lg">
-        {/* bg="light" */}
         <Container>
           <Navbar.Brand className= "text-white " href="/"><span style={{fontWeight: "bold"}}>gmoot</span></Navbar.Brand>
-          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end" style={{ width: "100%" }}>
               {props.walletAddress && <Nav.Link className="text-white" onClick={props.clicked}>{props.balance} SOL: {props.walletAddress}</Nav.Link>}
